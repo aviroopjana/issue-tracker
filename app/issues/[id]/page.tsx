cfb8753e7576344b00f7a2792ceb77c932f0fbb9
Aviroop Jana <aviroopjana@gmail.com>
2023-11-22 10:26:28 +0530

    chore: style the issue details page

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,4 +1,6 @@
+import IssueStatusBadge from "@/app/components/IssueStatusBadge";
 import prisma from "@/prisma/client";
+import { Card, Flex, Heading } from "@radix-ui/themes";
 import { notFound } from "next/navigation";
 import React from "react";
 
@@ -20,10 +22,12 @@ const IssueDetailsPage = async ({ params }: Props) => {
 
   return (
     <div>
-      <h1>{issue.name}</h1>
-      <p>{issue.description}</p>
-      <p>{issue.status}</p>
-      <p>{issue.createdAt.toDateString()}</p>
+      <Heading>{issue.name}</Heading>
+      <Flex className="space-x-3 mt-2 mb-5">
+        <IssueStatusBadge status={issue.status} /> 
+        <p>{issue.createdAt.toDateString()}</p>
+      </Flex>  
+      <Card className="max-w-2xl">{issue.description}</Card>  
     </div>
   );
 };
