fc95713e0680b25b1795c7921764206599467362
Aviroop Jana <aviroopjana@gmail.com>
2023-11-23 05:08:59 +0530

    feat: add the edit issue button

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,18 +1,19 @@
 import BackButton from "@/app/components/BackButton";
 import IssueStatusBadge from "@/app/components/IssueStatusBadge";
 import prisma from "@/prisma/client";
-import { Card, Flex, Heading } from "@radix-ui/themes";
+import { Box, Button, Card, Flex, Grid, Heading } from "@radix-ui/themes";
 import delay from "delay";
 import { notFound } from "next/navigation";
 import React from "react";
 import ReactMarkdown from "react-markdown";
+import { FaRegEdit } from "react-icons/fa";
+import Link from "next/link";
 
 interface Props {
   params: { id: string };
 }
 
 const IssueDetailsPage = async ({ params }: Props) => {
-
   const issue = await prisma.issue.findUnique({
     where: {
       id: parseInt(params.id),
@@ -26,17 +27,25 @@ const IssueDetailsPage = async ({ params }: Props) => {
   }
 
   return (
-    <div>
-    <BackButton href="/issues" />
-      <Heading>{issue.name}</Heading>
-      <Flex className="space-x-3 mt-2 mb-5">
-        <IssueStatusBadge status={issue.status} /> 
-        <p>{issue.createdAt.toDateString()}</p>
-      </Flex>  
-      <Card className="prose max-w-2xl">
-        <ReactMarkdown>{issue.description}</ReactMarkdown>    
-    </Card>  
-    </div>
+    <Grid columns={{ initial: "1", md: "2" }} gap={"5"}>
+      <Box>
+        <BackButton href="/issues" />
+        <Heading>{issue.name}</Heading>
+        <Flex className="space-x-3 mt-2 mb-5">
+          <IssueStatusBadge status={issue.status} />
+          <p>{issue.createdAt.toDateString()}</p>
+        </Flex>
+        <Card className="prose max-w-2xl">
+          <ReactMarkdown>{issue.description}</ReactMarkdown>
+        </Card>
+      </Box>
+      <Box className="mt-10">
+        <Button>
+          <FaRegEdit />
+          <Link href={`/issues/${issue.id}/edit`}>Edit Issue</Link>
+        </Button>
+      </Box>
+    </Grid>
   );
 };
 
