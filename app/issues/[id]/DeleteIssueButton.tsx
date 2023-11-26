0deb09e50a2eda9ecd5b907cad60c75004fc66d1
Aviroop Jana <aviroopjana@gmail.com>
2023-11-26 23:51:43 +0530

    feat: handling errors

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -2,51 +2,79 @@
 import { Button, Flex, AlertDialog } from "@radix-ui/themes";
 import axios from "axios";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 import { MdDelete } from "react-icons/md";
 
 const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
   const router = useRouter();
+  const [error, setError] = useState(false);
+
+  const deleteIssue = async () => {
+    try {
+      await axios.delete("/api/issues/" + issueId);
+      router.push("/issues");
+      router.refresh();
+    } catch (err) {
+      setError(true);
+      console.log(err);
+    }
+  };
 
   return (
-    <AlertDialog.Root>
-      <AlertDialog.Trigger>
-        <Button color="red" className="hover:cursor-pointer">
-          <MdDelete size={25} />
-          Delete Issue
-        </Button>
-      </AlertDialog.Trigger>
-      <AlertDialog.Content>
-        <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
-        <AlertDialog.Description>
-          Are you sure you want to delete this issue? This action can not be
-          undone.
-        </AlertDialog.Description>
-        <Flex className="mt-4 gap-4">
-          <AlertDialog.Cancel>
-            <Button
-              color="gray"
-              variant="soft"
-              className="hover:cursor-pointer"
-            >
-              Cancel
-            </Button>
-          </AlertDialog.Cancel>
-          <AlertDialog.Action>
-            <Button
-              color="red"
-              className="hover:cursor-pointer"
-              onClick={async () => {
-                await axios.delete("/api/issues/" + issueId);
-                router.push("/issues");
-                router.refresh();
-              }}
-            >
-              Delete Issue
-            </Button>
-          </AlertDialog.Action>
-        </Flex>
-      </AlertDialog.Content>
-    </AlertDialog.Root>
+    <>
+      <AlertDialog.Root>
+        <AlertDialog.Trigger>
+          <Button color="red" className="hover:cursor-pointer">
+            <MdDelete size={25} />
+            Delete Issue
+          </Button>
+        </AlertDialog.Trigger>
+        <AlertDialog.Content>
+          <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
+          <AlertDialog.Description>
+            Are you sure you want to delete this issue? This action can not be
+            undone.
+          </AlertDialog.Description>
+          <Flex className="mt-4 gap-4">
+            <AlertDialog.Cancel>
+              <Button
+                color="gray"
+                variant="soft"
+                className="hover:cursor-pointer"
+              >
+                Cancel
+              </Button>
+            </AlertDialog.Cancel>
+            <AlertDialog.Action>
+              <Button
+                color="red"
+                className="hover:cursor-pointer"
+                onClick={deleteIssue}
+              >
+                Delete Issue
+              </Button>
+            </AlertDialog.Action>
+          </Flex>
+        </AlertDialog.Content>
+      </AlertDialog.Root>
+      <AlertDialog.Root open={error}>
+        <AlertDialog.Content>
+          <AlertDialog.Title>Error</AlertDialog.Title>
+          <AlertDialog.Description>
+            There was an error deleting the issue. Please try again.
+          </AlertDialog.Description>
+          <Button
+            color="gray"
+            variant="soft"
+            mt="3"
+            className="hover:cursor-pointer"
+            onClick={() => setError(false)}
+          >
+            OK
+          </Button>
+        </AlertDialog.Content>
+      </AlertDialog.Root>
+    </>
   );
 };
 
